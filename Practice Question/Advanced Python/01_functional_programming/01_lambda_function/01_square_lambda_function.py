7db6363a30deef8c623a767efc074e16d7fd86e8
Leox27 <mayurx27@gmail.com>
2026-07-23 18:36:40 +0530

    Solved Que01: Square of a number using lambda function

---
+++ Practice Question/Advanced Python/01_functional_programming/01_lambda_function/01_square_lambda_function.py
@@ -0,0 +1,17 @@
+"""
+#1. (Lambda Function)
+
+Create a lambda function named square.
+It should accept one number
+and return its square.
+
+Call the lambda function with:
+10
+Print the result.
+"""
+
+square = lambda num : num*num
+print(square(10))
+'''
+100
+'''
